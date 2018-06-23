9cb7c6fa67c5a25783771ae4f1ff140813b1e1c1
eddel <eddeldanao@gmail.com>
2018-06-23 15:29:41 +0200

    home page

--- resources/views/home1.blade.php
+++ resources/views/home1.blade.php
@@ -1,30 +1,339 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
 
-        <title>Kraite Trading School</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>HTML Template</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'PT Sans',sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-        </style>
-    </head>
-    <body>
-    	<div class="content">
-    		
-    	</div>
-    </body>
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('css/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Owl Carousel -->
+    <link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/owl.theme.default.css') }}" rel="stylesheet">
+
+    <!-- Magnific Popup -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}" />
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/home.css') }}" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!-- Header -->
+    <header id="home">
+        <!-- Background Image -->
+        <div class="bg-img" style="background-image: url('/images/background1.jpg');">
+            <div class="overlay"></div>
+        </div>
+        <!-- /Background Image -->
+
+        <!-- Nav -->
+        <nav id="nav" class="navbar nav-transparent">
+            <div class="container">
+
+                <div class="navbar-header">
+                    <!-- Logo -->
+                    <div class="navbar-brand">
+                        <a href="index.html">
+                            <img class="logo" src="images/logo.png" alt="logo">
+                            <img class="logo-alt" src="images/logo.png" alt="logo">
+                        </a>
+                    </div>
+                    <!-- /Logo -->
+
+                    <!-- Collapse nav button -->
+                    <div class="nav-collapse">
+                        <span></span>
+                    </div>
+                    <!-- /Collapse nav button -->
+                </div>
+
+                <!--  Main navigation  -->
+                <ul class="main-nav nav navbar-nav navbar-right">
+                    <li><a href="#home">Home</a></li>
+                    <!-- <li><a href="#about">About</a></li> -->
+                    <!-- <li><a href="#portfolio">Portfolio</a></li> -->
+                    <!-- <li><a href="#service">Services</a></li> -->
+                    <!-- <li><a href="#pricing">Prices</a></li> -->
+                    <!-- <li><a href="#team">Team</a></li> -->
+                    <li><a href="#blog">Blog</a>
+                        <!-- <ul class="dropdown">
+                            <li><a href="blog-single.html">blog post</a></li>
+                        </ul> -->
+                    </li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <!-- /Main navigation -->
+
+            </div>
+        </nav>
+        <!-- /Nav -->
+
+        <!-- home wrapper -->
+        <div class="home-wrapper">
+            <div class="container">
+                <div class="row">
+
+                    <!-- home content -->
+                    <div class="col-md-10 col-md-offset-1">
+                        <div class="home-content">
+                            <h1 class="white-text">Kraite Trading School</h1>
+                            <p class="white-text">Trading is just a game and money is just paper.
+                            </p>
+                            <a class="white-btn" href="/login">Login</a>
+                            <a class="main-btn" href="/register">Register</a>
+                        </div>
+                    </div>
+                    <!-- /home content -->
+
+                </div>
+            </div>
+        </div>
+        <!-- /home wrapper -->
+
+    </header>
+    <!-- /Header -->
+
+    <!-- Blog -->
+    <div id="blog" class="section md-padding bg-grey">
+
+        <!-- Container -->
+        <div class="container">
+
+            <!-- Row -->
+            <div class="row">
+
+                <!-- Section header -->
+                <div class="section-header text-center">
+                    <h2 class="title">Recents news</h2>
+                </div>
+                <!-- /Section header -->
+
+                <!-- blog -->
+                <div class="col-md-4">
+                    <div class="blog">
+                        <div class="blog-img">
+                            <img class="img-responsive" src="/images/blog1.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <ul class="blog-meta">
+                                <li><i class="fa fa-user"></i>John doe</li>
+                                <li><i class="fa fa-clock-o"></i>18 Oct</li>
+                                <li><i class="fa fa-comments"></i>57</li>
+                            </ul>
+                            <h3>Molestie at elementum eu facilisis sed odio</h3>
+                            <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+                            <a href="blog-single.html">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /blog -->
+
+                <!-- blog -->
+                <div class="col-md-4">
+                    <div class="blog">
+                        <div class="blog-img">
+                            <img class="img-responsive" src="/images/blog2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <ul class="blog-meta">
+                                <li><i class="fa fa-user"></i>John doe</li>
+                                <li><i class="fa fa-clock-o"></i>18 Oct</li>
+                                <li><i class="fa fa-comments"></i>57</li>
+                            </ul>
+                            <h3>Molestie at elementum eu facilisis sed odio</h3>
+                            <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+                            <a href="blog-single.html">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /blog -->
+
+                <!-- blog -->
+                <div class="col-md-4">
+                    <div class="blog">
+                        <div class="blog-img">
+                            <img class="img-responsive"  src="/images/blog3.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <ul class="blog-meta">
+                                <li><i class="fa fa-user"></i>John doe</li>
+                                <li><i class="fa fa-clock-o"></i>18 Oct</li>
+                                <li><i class="fa fa-comments"></i>57</li>
+                            </ul>
+                            <h3>Molestie at elementum eu facilisis sed odio</h3>
+                            <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim nec.</p>
+                            <a href="blog-single.html">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /blog -->
+
+            </div>
+            <!-- /Row -->
+
+        </div>
+        <!-- /Container -->
+
+    </div>
+    <!-- /Blog -->
+
+    <!-- Contact -->
+    <div id="contact" class="section md-padding">
+
+        <!-- Container -->
+        <div class="container">
+
+            <!-- Row -->
+            <div class="row">
+
+                <!-- Section-header -->
+                <div class="section-header text-center">
+                    <h2 class="title">Get in touch</h2>
+                </div>
+                <!-- /Section-header -->
+
+                <!-- contact -->
+                <div class="col-sm-4">
+                    <div class="contact">
+                        <i class="fa fa-phone"></i>
+                        <h3>Phone</h3>
+                        <p>082-291-6483 <br>
+                           +639282143858 
+                        </p>
+                    </div>
+                </div>
+                <!-- /contact -->
+
+                <!-- contact -->
+                <div class="col-sm-4">
+                    <div class="contact">
+                        <i class="fa fa-envelope"></i>
+                        <h3>Email</h3>
+                        <p>kraite.hernandex@gmail.com</p>
+                    </div>
+                </div>
+                <!-- /contact -->
+
+                <!-- contact -->
+                <div class="col-sm-4">
+                    <div class="contact">
+                        <i class="fa fa-map-marker"></i>
+                        <h3>Address</h3>
+                        <p>Door #3 Apartment #007  <br>
+                            Alitaptap St. Lanzona Subdivision <br>
+                            Matina Aplaya Davao City
+                        </p>
+                    </div>
+                </div>
+                <!-- /contact -->
+
+                <!-- contact form -->
+                <div class="col-md-8 col-md-offset-2">
+                    <form class="contact-form">
+                        <input type="text" class="input" placeholder="Name">
+                        <input type="email" class="input" placeholder="Email">
+                        <input type="text" class="input" placeholder="Subject">
+                        <textarea class="input" placeholder="Message"></textarea>
+                        <button class="main-btn">Send message</button>
+                    </form>
+                </div>
+                <!-- /contact form -->
+
+            </div>
+            <!-- /Row -->
+
+        </div>
+        <!-- /Container -->
+
+    </div>
+    <!-- /Contact -->
+
+
+    <!-- Footer -->
+    <footer id="footer" class="sm-padding bg-dark">
+
+        <!-- Container -->
+        <div class="container">
+
+            <!-- Row -->
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <!-- footer logo -->
+                    <div class="footer-logo">
+                        <a href="/"><img src="images/logotext.png" alt="logo"></a>
+                    </div>
+                    <!-- /footer logo -->
+
+                    <!-- footer follow -->
+                    <ul class="footer-follow">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                    <!-- /footer follow -->
+
+                    <!-- footer copyright -->
+                    <div class="footer-copyright">
+                        <p>Copyright © 2017. All Rights Reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                    </div>
+                    <!-- /footer copyright -->
+
+                </div>
+
+            </div>
+            <!-- /Row -->
+
+        </div>
+        <!-- /Container -->
+
+    </footer>
+    <!-- /Footer -->
+
+    <!-- Back to top -->
+    <div id="back-to-top"></div>
+    <!-- /Back to top -->
+
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!-- /Preloader -->
+
+    <!-- jQuery Plugins -->
+    <script src="{{ asset('js/jQuery/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.magnific-popup.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/home.js') }}"></script>
+</body>
+
 </html>
