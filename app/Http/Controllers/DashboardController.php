a06ecfcd9d296b9cf06d2e219e47dbf3d32a361f
Jospeh G <developnow.415@gmail.com>
2018-06-30 01:40:06 +0200

    fixed dashboard error

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -45,7 +45,7 @@ class DashboardController extends Controller
         self::$details['data']['daily_rankings'] = self::getUsers()->each(function($user) {
             $profit_today       = $user->performances()->where('date', self::$details['date'])->value('profit');
             $equity_yesterday   = $user->available_equity - $profit_today;
-            $user->daily_change = pround(($profit_today / $equity_yesterday) * 100);
+            $user->daily_change = ($profit_today)? pround(($profit_today / $equity_yesterday) * 100): 0;
             $user->date         = _date(self::$details['date']);
         })->sortByDesc('daily_change');
     }
@@ -55,7 +55,7 @@ class DashboardController extends Controller
         self::$details['data']['weekly_rankings'] = self::getUsers()->each(function($user) {
             $profit_this_week    = $user->performances()->where(DB::raw("WEEK(date)"), self::$details['week'])->value('profit');
             $equity_last_week    = $user->available_equity - $profit_this_week;
-            $user->weekly_change = pround(($profit_this_week / $equity_last_week) * 100);
+            $user->weekly_change = ($profit_this_week)? pround(($profit_this_week / $equity_last_week) * 100): 0;
             $user->week          = self::$details['week'];
         })->sortByDesc('weekly_change');
     }
@@ -64,8 +64,8 @@ class DashboardController extends Controller
     {
         self::$details['data']['monthly_rankings'] = self::getUsers()->each(function($user) {
             $profit_this_month    = $user->performances()->where(DB::raw("WEEK(date)"), self::$details['week'])->value('profit');
-            $equity_last_month   = $user->available_equity - $profit_this_month;
-            $user->monthly_change = pround(($profit_this_month / $equity_last_month) * 100);
+            $equity_last_month    = $user->available_equity - $profit_this_month;
+            $user->monthly_change = ($profit_this_month)? pround(($profit_this_month / $equity_last_month) * 100): 0;
             $user->month          = month(self::$details['month']);
         })->sortByDesc('monthly_change');
     }
