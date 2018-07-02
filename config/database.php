6e8deed1eb7ba83dc266ba991de8790cc9743719
Jospeh G <developnow.415@gmail.com>
2018-07-03 00:46:27 +0200

    fixed error

--- config/database.php
+++ config/database.php
@@ -54,21 +54,21 @@ return [
             'engine' => null,
         ],
 
-        'mysql1' => [
-            'driver' => 'mysql',
-            'port' => env('DB_PORT', '3306'),
-            'host' => env('DB_HOST', 'sql7.freemysqlhosting.net'),
-            'database' => env('DB_DATABASE', 'sql7245493'),
-            'username' => env('DB_USERNAME', 'sql7245493'),
-            'password' => env('DB_PASSWORD', 'tpRfaMr9za'),
+        // 'mysql1' => [
+        //     'driver' => 'mysql',
+        //     'port' => env('DB_PORT', '3306'),
+        //     'host' => env('DB_HOST', 'sql7.freemysqlhosting.net'),
+        //     'database' => env('DB_DATABASE', 'sql7245493'),
+        //     'username' => env('DB_USERNAME', 'sql7245493'),
+        //     'password' => env('DB_PASSWORD', 'tpRfaMr9za'),
                         
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'strict' => true,
+        //     'engine' => null,
+        // ],
 
         'pgsql' => [
             'driver' => 'pgsql',
